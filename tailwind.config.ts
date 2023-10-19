946fb4bcef0880f8b8a122efac98eb6af3c12e12
walacefex <walacefex01@gmail.com>
2023-10-18 22:30:28 -0300

    creating variable color green

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,11 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: { },
+    extend: {
+      colors: {
+        'green-primary': '#2A4B46',
+      },
+     },
   },
   plugins: [],
 }
