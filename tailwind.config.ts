2ab707224623466e36db70b6745be25e811cd139
walacefex <walacefex01@gmail.com>
2023-10-19 00:29:54 -0300

    adding specific size to area cards

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,6 +20,7 @@ const config: Config = {
         'text-hero': '66rem',
         'area-icons': '53.4375rem',
         'area-mockups': '59.8125rem',
+        'area-cards': '82.5rem',
       },
       height: {
         'section-hero': '54.625rem',
